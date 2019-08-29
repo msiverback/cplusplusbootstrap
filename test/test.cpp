085bc4de63a35350b9f88d72f7f7cacda50f8808
Martin Siverbäck <martin.siverback@combitech.se>
2019-08-29 15:35:40 +0200

    Fixed the code files.

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : FizzBuzz.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -18,26 +18,8 @@ using namespace FB;
 TEST(fizzBuzz, RegularNumbers) {
   FizzBuzz fizzBuzz;
   ASSERT_EQ("1", fizzBuzz.getFizzBuzzNumber(1));
-  ASSERT_EQ("2", fizzBuzz.getFizzBuzzNumber(2));
 }
 
-TEST(fizzBuzz, FizzNumbers) {
-	FizzBuzz fizzBuzz;
-	  ASSERT_EQ("fizz", fizzBuzz.getFizzBuzzNumber(3));
-	  ASSERT_EQ("fizz", fizzBuzz.getFizzBuzzNumber(9));
-}
-
-TEST(fizzBuzz, BuzzNumbers) {
-	FizzBuzz fizzBuzz;
-	  ASSERT_EQ("buzz", fizzBuzz.getFizzBuzzNumber(5));
-	  ASSERT_EQ("buzz", fizzBuzz.getFizzBuzzNumber(25));
-}
-
-TEST(fizzBuzz, FizzBuzzNumbers){
-	FizzBuzz fizzBuzz;
-	  ASSERT_EQ("fizzbuzz", fizzBuzz.getFizzBuzzNumber(15));
-	  ASSERT_EQ("fizzbuzz", fizzBuzz.getFizzBuzzNumber(30));
-}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
